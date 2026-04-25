f5125e6e9c317081070fd10d7395aca93b0a232a
DESKTOP-FSAT3VU\post <post0813@naver.com>
2026-04-25 19:10:07 +0900

    no message

--- MFCApplication1/MFCApplication1/MFCApplication1Dlg.cpp
+++ MFCApplication1/MFCApplication1/MFCApplication1Dlg.cpp
@@ -173,12 +173,13 @@ void CMFCApplication1Dlg::OnBnClickedButton2()
 void CMFCApplication1Dlg::OnBnClickedButton5()
 {
 	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
-	AfxMessageBox(_T("OnBnClickedButton5"));
+	// A-0
+	// B-0
 }
 
 void CMFCApplication1Dlg::OnBnClickedButton6()
 {
 	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
 	// A-1
-	// B-0
+	// B-2
 }
