a2fb138ad7cd002d14b229d9865452b52b12d4d2
DESKTOP-9VR1M0J\post <post0813@naver.com>
2026-04-25 13:03:07 +0900

    no message

--- MFCApplication1/MFCApplication1/Resource.h
+++ MFCApplication1/MFCApplication1/Resource.h
@@ -10,7 +10,8 @@
 #define IDC_BUTTON1                     1000
 #define IDC_BUTTON2                     1001
 #define IDC_BUTTON3                     1002
-#define IDC_BUTTON4                     1002
+#define IDC_BUTTON4                     1003
+
 
 // Next default values for new objects
 // 
