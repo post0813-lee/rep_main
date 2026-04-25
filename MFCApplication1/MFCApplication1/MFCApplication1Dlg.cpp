42631152d2a3b1d823910db8fcb875ae911b1eed
DESKTOP-9VR1M0J\post <post0813@naver.com>
2026-04-25 13:12:17 +0900

    no message

--- MFCApplication1/MFCApplication1/MFCApplication1Dlg.cpp
+++ MFCApplication1/MFCApplication1/MFCApplication1Dlg.cpp
@@ -67,6 +67,7 @@ BEGIN_MESSAGE_MAP(CMFCApplication1Dlg, CDialogEx)
 	ON_WM_QUERYDRAGICON()
 	ON_BN_CLICKED(IDC_BUTTON1, &CMFCApplication1Dlg::OnBnClickedButton1)
 	ON_BN_CLICKED(IDC_BUTTON2, &CMFCApplication1Dlg::OnBnClickedButton2)
+	ON_BN_CLICKED(IDC_BUTTON5, &CMFCApplication1Dlg::OnBnClickedButton5)
 END_MESSAGE_MAP()
 
 
@@ -166,4 +167,9 @@ void CMFCApplication1Dlg::OnBnClickedButton2()
 {
 	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
 	AfxMessageBox(_T("OnBnClickedButton2"));
+}
+
+void CMFCApplication1Dlg::OnBnClickedButton5()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
 }
