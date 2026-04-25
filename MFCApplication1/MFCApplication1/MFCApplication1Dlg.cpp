873c3297195aa919024d3059b234fc5a186f6e6b
DESKTOP-FSAT3VU\post <post0813@naver.com>
2026-04-25 21:32:53 +0900

    no message

--- MFCApplication1/MFCApplication1/MFCApplication1Dlg.cpp
+++ MFCApplication1/MFCApplication1/MFCApplication1Dlg.cpp
@@ -179,6 +179,16 @@ void CMFCApplication1Dlg::OnBnClickedButton5()
 void CMFCApplication1Dlg::OnBnClickedButton6()
 {
 	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	// ds
+	// ds
+	// ds
+	// ds
+	// d
+	// sd
+	// s
+	// d
+	// d
+	// s
 	// B-4
 	// B-4
 	// B-4
