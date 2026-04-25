c7236981549847833bf5fc37387dd3066c5b7035
DESKTOP-FSAT3VU\post <post0813@naver.com>
2026-04-25 13:09:58 +0900

    no message

--- MFCApplication1/MFCApplication1/Resource.h
+++ MFCApplication1/MFCApplication1/Resource.h
@@ -11,6 +11,8 @@
 #define IDC_BUTTON2                     1001
 #define IDC_BUTTON3                     1002
 #define IDC_BUTTON4                     1003
+#define IDC_BUTTON5                     1004
+#define IDC_BUTTON6                     1005
 
 // Next default values for new objects
 // 
@@ -18,7 +20,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
