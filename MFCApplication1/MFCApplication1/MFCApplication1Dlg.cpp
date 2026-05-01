80fc2c6ffe22339b66ad411571f489f8f80b8c97
post0813-lee <protos0214@gmail.com>
2026-05-01 20:39:05 +0900

    Hme 1234 (#2)
    * KME_5678
    
    * KME_5678_2
    
    * KME_5678_4
    
    * KME_5678
    
    * no message
    
    * no message
    
    * 0927
    
    * [dev-EU] 버전 1
    
    * [dev-EU] 버전 1
    
    * // 버전 2
    
    * // 버전 3
    
    * // 버전 4
    
    * Revert "// 버전 4"
    
    This reverts commit e9765d22db6a04e90e7fb1942ef6b3a936dcb983.
    
    * // 버전 5
    
    * // 버전 1
    
    * // 버전 2
    
    * // 버전 3
    
    * // 버전 4
    
    * // 버전 5
    
    * // 버전 6 - 이상규
    
    * no message
    
    * // 버전 10 - 이석진
    
    ---------
    
    Co-authored-by: DESKTOP-9VR1M0J\post <post0813@naver.com>

--- MFCApplication1/MFCApplication1/MFCApplication1Dlg.cpp
+++ MFCApplication1/MFCApplication1/MFCApplication1Dlg.cpp
@@ -175,7 +175,7 @@ void CMFCApplication1Dlg::OnBnClickedButton5()
 	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
 	// HME_1234_8
 }
-hhhhh
+
 void CMFCApplication1Dlg::OnBnClickedButton6()
 {
 	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
