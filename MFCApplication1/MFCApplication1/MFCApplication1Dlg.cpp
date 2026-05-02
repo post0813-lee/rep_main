a194d99898c6c4e337831ac35e3a6025cf55f7e8
DESKTOP-9VR1M0J\post <post0813@naver.com>
2026-05-02 11:46:58 +0900

    실수로 지움 - HME_234

--- MFCApplication1/MFCApplication1/MFCApplication1Dlg.cpp
+++ MFCApplication1/MFCApplication1/MFCApplication1Dlg.cpp
@@ -200,12 +200,6 @@ void CMFCApplication1Dlg::OnBnClickedButton6()
 // 버전 11 - dev-EU
 // 버전 12 - dev-EU
 // 버전 13 - dev-EU
-// 버전 14 - dev-EU
-// 버전 15 - dev-EU
-// 버전 16 - dev-EU
-// 버전 17 - dev-EU
-// 버전 18 - dev-EU
-// 버전 19 - dev-EU
 
 // 버전 20 - HME_5678
 // 버전 21 - HME_5678-EU- ffgffff
